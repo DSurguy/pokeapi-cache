cec9ce92bd7d3000c070aa47fd2a61429e3fa760
dsurguy-rn <dsurguy@researchnow.com>
2017-12-05 17:03:06 -0600

    Progress on creating docker-compose.yml from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# check for existance of setup.env and load vars from it
+# check for existance of setup.env and load vars from it, also back it up
 if [ -e setup.env ]
 then
+  # simple backup
+  cp setup.env setup.env.bak
   source ./setup.env
 fi
 
 # define all env keys and defaults
-KEYS=("POKEAPI_MONGO_HOST" "POKEAPI_MONGO_PORT" "POKEAPI_MONGO_DB" "POKEAPI_MONGO_USER" "POKEAPI_MONGO_PASS")
-VALUES=($POKEAPI_MONGO_HOST $POKEAPI_MONGO_PORT $POKEAPI_MONGO_DB $POKEAPI_MONGO_USER $POKEAPI_MONGO_PASS)
-DEFAULTS=('localhost' '27107' 'poke' )
+KEYS=("POKEAPI_APP_PORT" "POKEAPI_MONGO_HOST" "POKEAPI_MONGO_PORT" "POKEAPI_MONGO_DB" "POKEAPI_MONGO_USER" "POKEAPI_MONGO_PASS")
+VALUES=($POKEAPI_APP_PORT $POKEAPI_MONGO_HOST $POKEAPI_MONGO_PORT $POKEAPI_MONGO_DB $POKEAPI_MONGO_USER $POKEAPI_MONGO_PASS)
+DEFAULTS=('8080' 'localhost' '27107' 'poke' )
 
 #FUNCTION promptVar
 # Prompt the user for a key, displaying current or default value
@@ -43,3 +45,64 @@ done;
 for i in "${!KEYS[@]}"; do 
   echo "${KEYS[$i]}=${VALUES[$i]}" >> setup.env
 done
+
+# Reload env vars
+source ./setup.env
+
+#clone and overwrite docker-compose file
+if [ -e docker-compose.yml ]
+then
+  cp docker-compose.yml docker-compose.yml.bak
+fi
+> docker-compose.yml
+
+# Begin write-out
+echo "version: \"2\"
+services:
+  pokeapi-cache-app:
+    container_name: pokeapi-cache-app
+    restart: always
+    build: .
+    environment:
+      - POKEAPI_MONGO_HOST: ${POKEAPI_MONGO_HOST}
+      - POKEAPI_MONGO_PORT: ${POKEAPI_MONGO_PORT}
+      - POKEAPI_MONGO_DB: ${POKEAPI_MONGO_DB}" >> docker-compose.yml
+if [ -z ${POKEAPI_MONGO_USER+x} ]
+then
+  echo "       - POKEAPI_MONGO_USER: ${POKEAPI_MONGO_USER}" >> docker-compose.yml
+fi
+if [ -z ${POKEAPI_MONGO_PASS+x} ]
+then
+  echo "       - POKEAPI_MONGO_PASS: ${POKEAPI_MONGO_PASS}" >> docker-compose.yml
+fi
+echo "     ports:
+      - \"${POKEAPI_APP_PORT}:${POKEAPI_APP_PORT}\"" >> docker-compose.yml
+
+while true; do
+  read -p "Run mongo in docker? (y/n): " yn
+  case $yn in
+    [yY]* ) yn=true; break;;
+    [nN]* ) yn=false; break;;
+    * ) echo -ne "\033[1A\r\033[K";
+  esac
+done
+if [ yn==true ]
+then
+  echo "   pokeapi-cache-mongo:
+      container_name: pokeapi-cache-mongo
+      restart: always
+      image: mongo
+      environment:" >> docker-compose.yml
+  if [ -z ${POKEAPI_MONGO_USER+x} ]
+  then
+    echo "      MONGO_INITDB_ROOT_USERNAME: ${POKEAPI_MONGO_USER}" >> docker-compose.yml
+  fi
+  if [ -z ${POKEAPI_MONGO_PASS+x} ]
+  then
+    echo "      MONGO_INITDB_ROOT_PASSWORD: ${POKEAPI_MONGO_PASS}" >> docker-compose.yml
+  fi
+  echo "    volumes:
+      - ./_data:/data/db
+    ports:
+      - \"${POKEAPI_MONGO_PORT}:27107\"" >> docker-compose.yml
+fi
